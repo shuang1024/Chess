129f34a26e7d5e49c1a2e1742eab21ae5ab813cb
yellowfish-67 <yusongh2008@gmail.com>
2021-06-18 12:44:22 -0700

    Turned pieces from list to dictionary

--- src/constants.py
+++ src/constants.py
@@ -10,9 +10,9 @@ LIGHT = (80, 165, 80)
 
 SQ_SIZE = 100
 
-pieces = []
+pieces = {}
 
 for f in ("p", "n", "b", "r", "q", "k"):
     for l in ("w", "b"):
         img = pygame.transform.scale(pygame.image.load(os.path.join("imgs", f + l + ".png")), (SQ_SIZE, SQ_SIZE))
-        pieces.append(img)
+        pieces[f + l] = img
