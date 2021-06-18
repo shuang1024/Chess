575c462a1628c5df63c67b5df04bf449c8c541a8
yellowfish-67 <yusongh2008@gmail.com>
2021-06-18 12:43:17 -0700

    Added images to list

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,6 @@
+import os
+import pygame
+
 WIDTH, HEIGHT = 800, 800
 FPS = 60
 
@@ -6,3 +9,10 @@ DARK = (50, 50, 50)
 LIGHT = (80, 165, 80)
 
 SQ_SIZE = 100
+
+pieces = []
+
+for f in ("p", "n", "b", "r", "q", "k"):
+    for l in ("w", "b"):
+        img = pygame.transform.scale(pygame.image.load(os.path.join("imgs", f + l + ".png")), (SQ_SIZE, SQ_SIZE))
+        pieces.append(img)
